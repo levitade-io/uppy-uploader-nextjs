687306739bbc3314151ab4290ba5e7ed979607e7
Bryan de Jong <bryandejong99@gmail.com>
2024-06-15 13:30:40 +0200

    fix: semantic-release process

--- release.config.js
+++ release.config.js
@@ -3,9 +3,13 @@ module.exports = {
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
-        '@semantic-release/changelog',
         '@semantic-release/npm',
-        '@semantic-release/github',
-        '@semantic-release/git',
+        ['@semantic-release/git',
+            {
+                assets: ['CHANGELOG.md', 'package.json', 'dist/'],
+                message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+            }
+        ],
+        '@semantic-release/github'
     ]
 };
